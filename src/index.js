e0c8c2124e894648598bffeb86990e6353024222
PC_ilya <kudienko2016@mail.ru>
2024-05-06 16:27:26 +0500

    вторая попытка

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.scss";
 import { Provider } from "react-redux";
 import store from "./store/store";
